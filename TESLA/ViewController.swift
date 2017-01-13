e68c97f89a2603035fe949a1e667c9e8780aab7a
Oldrin <oldrin@qburst.com>
2017-01-13 21:02:15 +0530

    setup camera

--- TESLA/ViewController.swift
+++ TESLA/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cameraView: UIView!
+    
+    var captureSession:AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        self.setupCameraSession()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func setupCameraSession() {
+        
+        self.captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSessionPresetHigh
+        
+        let backCamera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        if let input = try? AVCaptureDeviceInput(device: backCamera) {
+            
+            self.captureSession.addInput(input)
+        }
+        
+        self.previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        
+        self.previewLayer.frame = self.cameraView.bounds
+        self.previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        self.previewLayer.connection.videoOrientation = AVCaptureVideoOrientation.landscapeLeft
+        
+        cameraView.layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
+        
+    }
 
 }
 
